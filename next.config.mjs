03c934969b0b19623b0fa822f3c1212026ea98b5
Denys Reshetnyk <denus.reshetnuk@gmail.com>
2024-10-12 23:01:59 +0300

    add output type

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     webpack(config) {
         // Grab the existing rule that handles SVG imports
